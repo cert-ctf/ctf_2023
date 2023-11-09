ca768ae8c93627590e896c5d2593b2a288091aaa
cert-ctf <tobias.kirschke@gematik.de>
2023-11-09 15:12:25 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
+let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
@@ -18,6 +19,9 @@ WA.onInit().then(() => {
 	WA.room.area.onLeave('Mirror_Duke').subscribe(() => {
 		WA.room.hideLayer("decoration/mirror_duke");		
 	}) 
+	
+	
+	//Roofs------------------------------------------------------------------------
 
 	// HiFi Room
 	WA.room.area.onEnter('HiFi_Room').subscribe(() => {
@@ -64,18 +68,87 @@ WA.onInit().then(() => {
 		WA.room.showLayer("Bus_Station_roof");		
 	}) 
 	
+	
+	//SPAWN TIM------------------------------------------------------------------------
 	// Tim CarCinema
-	WA.room.area.onEnter('tim_01').subscribe(() => {
+	WA.room.area.onEnter('tim_01').subscribe(() => {	
 		WA.room.setTiles([		
 		  { x: 76, y: 12, tile: "tim", layer: "Stuff_6" },
+		  { x: 76, y: 11, tile: "tim_info_01a", layer: "Above_1" },
+		  { x: 76, y: 12, tile: "tim_info_01b", layer: "Above_1" },		  
 		]);	
 	})
 	
 	WA.room.area.onLeave('tim_01').subscribe(() => {
 		WA.room.setTiles([		
 		  { x: 76, y: 12, tile: null, layer: "Stuff_6" },
+		  { x: 76, y: 11, tile: null, layer: "Above_1" },
+		  { x: 76, y: 12, tile: null, layer: "Above_1" },		  
 		]);	
 	}) 
+	
+	// Tim Bridge
+	WA.room.area.onEnter('tim_02').subscribe(() => {
+		WA.room.setTiles([		
+		  { x: 75, y: 39, tile: "tim", layer: "Stuff_6" },
+		  { x: 75, y: 38, tile: "tim_info_01a", layer: "Above_1" },
+		  { x: 75, y: 39, tile: "tim_info_01b", layer: "Above_1" },		  
+		]);	
+	})
+	
+	WA.room.area.onLeave('tim_02').subscribe(() => {
+		WA.room.setTiles([		
+		  { x: 75, y: 39, tile: null, layer: "Stuff_6" },
+		  { x: 75, y: 38, tile: null, layer: "Above_1" },
+		  { x: 75, y: 39, tile: null, layer: "Above_1" },		  
+		]);	
+	}) 
+	
+	
+	//POPUPS---------------------------------------------------------------------------
+	
+	let helloWorldPopup;
+
+	// Open the popup when we enter a given zone
+	WA.room.onEnterLayer("tim_cinema").subscribe(() => {
+		console.log("HELLO")
+		helloWorldPopup = WA.ui.openPopup("Popup_tim_cinema", 'Hello world!', [{
+			label: "Close",
+			className: "primary",
+			callback: (popup) => {
+				// Close the popup when the "Close" button is pressed.
+				popup.close();
+			}
+		}]);
+	});
+
+	// Close the popup when we leave the zone.
+	WA.room.onLeaveLayer("tim_cinema").subscribe(() => {
+		helloWorldPopup.close();
+	})
+	
+	// TIM Cinema
+	/*WA.room.area.onEnter("tim_cinema").subscribe(() => {
+		currentPopup = WA.ui.openPopup("Popup_tim_cinema", 'Hhlhcl, tcpt trol fsl FTE 🐶',[]);
+	});
+	
+	WA.room.area.onLeave('tim_cinema').subscribe(() => {
+		currentPopup.close();
+	}) */
+	
+	// TIM Hifi
+	WA.room.area.onEnter("tim_hifi").subscribe(() => {
+		currentPopup = WA.ui.openPopup("Popup_tim_hifi", 'ecast kicmtk Ea ropk rsraac cqxmhieea 🐶💬📲',[]);
+	});
+	
+	WA.room.area.onLeave('tim_hifi').subscribe(() => {
+		currentPopup.close();
+	}) 
+	
+
+
+
+
 	
 
 
